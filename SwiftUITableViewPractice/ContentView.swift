b798411b147028866ada0a6b8b7be61f29f6726c
ryoya.fuseya <fuguliangmi@gmail.com>
2021-07-22 05:29:44 +0900

    Initial Commit

---
+++ SwiftUITableViewPractice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITableViewPractice
+//
+//  Created by 伏谷亮弥 on 2021/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUITableViewPractice/SwiftUITableViewPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITableViewPracticeApp.swift
+//  SwiftUITableViewPractice
+//
+//  Created by 伏谷亮弥 on 2021/07/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITableViewPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
